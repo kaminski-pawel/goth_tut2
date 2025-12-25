305f2e39ef8ab95290966b99df7dac11aa774482
Pawel Kaminski <29786902+kaminski-pawel@users.noreply.github.com>
2025-12-25 12:57:07 +0100

    Move data to models module

--- models/country.go
+++ models/country.go
@@ -1,12 +1,33 @@
 package models
 
-// "github.com/kaminski-pawel/goth-tut1/models"
-
 type Country struct {
     Name string
     Capital string
-    Population int
+    Population int64
     Continent string
     Description string
 }
 
+var Countries = []Country{
+    {
+	Name: "Japan",
+	Capital: "Tokyo",
+	Population: 125700000,
+	Continent: "Asia",
+	Description: "Uwu",
+    },
+    {
+	Name: "Brazil",
+	Capital: "Brasilia",
+	Population: 2143000000,
+	Continent: "South America",
+	Description: "Brazil mentioned",
+    },
+    {
+	Name: "France",
+	Capital: "Paris",
+	Population: 67390000,
+	Continent: "Europe",
+	Description: "Fois gras",
+    },
+}
