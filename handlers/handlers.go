b6c30b28d6f32a0062423bc83401dd24d5c276e4
Pawel Kaminski <29786902+kaminski-pawel@users.noreply.github.com>
2025-12-31 01:02:23 +0100

    Format handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,37 +1,37 @@
 package handlers
 
 import (
-    "strings"
-    "net/http"
+	"net/http"
+	"strings"
 
-    "github.com/kaminski-pawel/goth_tut2/components"
-    "github.com/kaminski-pawel/goth_tut2/models"
+	"github.com/kaminski-pawel/goth_tut2/components"
+	"github.com/kaminski-pawel/goth_tut2/models"
 )
 
 func CountryList(w http.ResponseWriter, r *http.Request) {
-    component := components.Countries(models.Countries)
-    component.Render(r.Context(), w)
+	component := components.Countries(models.Countries)
+	component.Render(r.Context(), w)
 }
 
 func CountryDetail(w http.ResponseWriter, r *http.Request) {
-    name := r.PathValue("name")
-    for _, country := range models.Countries {
-	if country.Name == name {
-	    component := components.CountryDetail(country)
-	    component.Render(r.Context(), w)
-	    return
+	name := r.PathValue("name")
+	for _, country := range models.Countries {
+		if country.Name == name {
+			component := components.CountryDetail(country)
+			component.Render(r.Context(), w)
+			return
+		}
 	}
-    }
-    http.NotFound(w, r)
+	http.NotFound(w, r)
 }
 
 func SearchCountries(w http.ResponseWriter, r *http.Request) {
-    query := strings.ToLower(r.URL.Query().Get("search"))
-    var results []models.Country
-    for _, country := range models.Countries {
-	if strings.Contains(strings.ToLower(country.Name), query) {
-	    results = append(results, country)
+	query := strings.ToLower(r.URL.Query().Get("search"))
+	var results []models.Country
+	for _, country := range models.Countries {
+		if strings.Contains(strings.ToLower(country.Name), query) {
+			results = append(results, country)
+		}
 	}
-    }
-    components.CountryList(results).Render(r.Context(), w)
+	components.CountryList(results).Render(r.Context(), w)
 }
